f50773ed433f3ef2978a743b9b75b0c7ef6129ec
Manuel Viozquez <manviocam@alu.edu.gva.es>
2024-10-22 19:54:47 +0200

    Practica realizada hasta la creacion del objeto Gasto.

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,33 @@
+'use strict';
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto = 0;
 
-
-function actualizarPresupuesto() {
+function actualizarPresupuesto(valor) {
     // TODO
+        if(isNaN(valor)){
+        return  -1;
+        console.log("El valor indicado no es un número.")
+    } else if (valor<0) {
+        return  -1;
+        console.log("El valor ha de ser mayor que 0.")
+    } 
+    presupuesto = valor;
+
+    return presupuesto;
 }
 
 function mostrarPresupuesto() {
     // TODO
+    let texto= 'Tu presupuesto actual es de ${presupuesto} €.';
+    return texto;
 }
 
-function CrearGasto() {
+function CrearGasto(descripcion,valor) {
     // TODO
+        this.descripcion=descripcion;
+        this.valor= (valor>=0) ? valor : 0 ;
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
