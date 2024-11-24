42958576662f114dc4398cef245bc3ee8e8d923a
Manuel Viozquez <manviocam@alu.edu.gva.es>
2024-11-24 19:53:35 +0100

    mostrar presupuesto

--- js/generarDatosEstadisticos.js
+++ js/generarDatosEstadisticos.js
@@ -1,3 +1,7 @@
 'use stirct';
 import * as gestionPresupuesto from "./gestionPresupuesto.js";
 import * as gestionPresupuestoWeb from "./gestionPresupuestoWeb.js";
+
+gestionPresupuesto.actualizarPresupuesto(1500);
+gestionPresupuestoWeb.mostrarDatoEnId("presupuesto", gestionPresupuesto.mostrarPresupuesto());
+
