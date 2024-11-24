90b20f8a5b4ff6e1ee7c97f7d26381731387c096
Manuel Viozquez <manviocam@alu.edu.gva.es>
2024-11-24 20:01:40 +0100

    añadir gastos y calcular tota y balance

--- js/generarDatosEstadisticos.js
+++ js/generarDatosEstadisticos.js
@@ -5,3 +5,18 @@ import * as gestionPresupuestoWeb from "./gestionPresupuestoWeb.js";
 gestionPresupuesto.actualizarPresupuesto(1500);
 gestionPresupuestoWeb.mostrarDatoEnId("presupuesto", gestionPresupuesto.mostrarPresupuesto());
 
+let gastos = [
+    gestionPresupuesto.crearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida"),
+    gestionPresupuesto.crearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida"),
+    gestionPresupuesto.crearGasto("Bonobús", 18.60, "2020-05-26", "transporte"),
+    gestionPresupuesto.crearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina"),
+    gestionPresupuesto.crearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros"),
+    gestionPresupuesto.crearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros"),
+];
+
+/*gestionPresupuesto.anyadirGastos(gastos);*/
+gastos.forEach(gasto => gestionPresupuesto.anyadirGasto(gasto));
+
+
+gestionPresupuestoWeb.mostrarDatoEnId("gastos-totales", gestionPresupuesto.calcularTotalGastos());
+gestionPresupuestoWeb.mostrarDatoEnId("balance-total", gestionPresupuesto.calcularBalance());
