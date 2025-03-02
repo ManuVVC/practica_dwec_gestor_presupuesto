78d292f7166ba52fb6d8a395212318372aece883
Manuel Viozquez <manviocam@alu.edu.gva.es>
2025-03-02 15:29:37 +0100

    arreglar las fechas en los filtros

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -11,12 +11,13 @@ function mostrarDatoEnId(idElemento, valor) {
 
 function mostrarGastoWeb(idElemento,gasto) {    
     let elemento = document.getElementById(idElemento);
+    let fecha = new Date(gasto.fecha);
     if (elemento) {
         let divGasto = document.createElement('div');
         divGasto.className='gasto';
         divGasto.innerHTML = `
             <div class="gasto-descripcion">${gasto.descripcion}</div>
-            <div class="gasto-fecha">${gasto.fecha}</div>
+            <div class="gasto-fecha">${fecha.toLocaleDateString('es')}</div>
             <div class="gasto-valor">${gasto.valor}€</div>
             <div class="gasto-etiquetas">
                 ${gasto.etiquetas.map(etiqueta => `<span class="gasto-etiquetas-etiqueta">${etiqueta}</span>`).join("")}
@@ -41,7 +42,7 @@ function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
             let divDato = document.createElement("div");
             divDato.className = "agrupacion-dato";
             divDato.innerHTML = `
-                <span class="agrupacion-dato-clave">${clave} -- </span>
+                <span class="agrupacion-dato-clave">${clave}</span>
                 <span class="agrupacion-dato-valor">${valor}€</span>
             `;
             divAgrupacion.appendChild(divDato);
